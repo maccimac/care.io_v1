ff2f99ad6dcd297af74445c4cb99f72dad57516b
maccimac <macci.web@gmail.com>
2022-09-30 20:30:10 -0700

    apply datepicker

--- frontend/js/booking.js
+++ frontend/js/booking.js
@@ -1,64 +1,8 @@
 import * as Config from './config.js'
 import * as Utils from './utils.js'
+import * as jq from './jquery.js'
 
 export const includeModalBooking = async () =>{
     document.getElementById('CareIOModalBooking').innerHTML = await Utils.getHtml('/frontend/modules/modal-booking.html')
-
-    // const bookingBtns = document.querySelectorAll('.care_trigger--booking')
-    // bookingBtns.forEach(bookingBtn => {
-    //     addEventListener('click', generateToken)
-    // });
-    
-    
-    // document.querySelector('#CareIOModalLogin #submitLogin').addEventListener(
-    //     'click', onSubmit
-    // )
+    jq.setupDatePicker()
 }
-
-// const generateToken = async () => {
-//     try{
-//         const res = await fetch(`${Config.HOST}/hello`, {
-//             method: 'GET',
-//             mode: 'cors',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-//                 'Access-Control-Allow-Headers':'Content-Type, Authorization, X-Requested-With'
-//             }
-            
-//         })
-//         console.log(res);
-//     }catch(err){
-//         console.log(err.message)
-//     }
-
-// }
-
-// const onSubmit = async (evt) => {
-//     evt.preventDefault()
-//     const userString = document.querySelector('#CareIOModalLogin #inputUsername').value
-//     const pwString = document.querySelector('#CareIOModalLogin #inputPassword').value
-//     console.log({userString, pwString})
-//     try{
-//         const res = await fetch(`${Config.HOST}/login`, {
-//             method: 'POST',
-//             mode: 'cors',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-//                 'Access-Control-Allow-Headers':'Content-Type, Authorization, X-Requested-With'
-//             },
-//             body:{
-//                 user: userString,
-//                 password: pwString
-//             }
-            
-//         })
-//         console.log(res);
-//     }catch(err){
-//         console.log(err.message)
-//     }
-    
-// }
--- frontend/js/jquery.js
+++ frontend/js/jquery.js
@@ -1,5 +1,34 @@
-// $(document).ready(function(){
-//     alert('jq test')
-//     $("#modalBooking").modal("show");
+export const setupDatePicker = () =>{
+    if($("#modalBooking")){
+        console.log('::careio:: modal booking found')
+        // @url: https://jqueryui.com/datepicker/
+        // @url: https://formden.com/blog/date-picker
+        // @url: https://bootstrap-datepicker.readthedocs.io/en/stable/
+        const container = "#careio-datepicker"
+        
+        const options = {
+            todayHighlight: true,
+            autoclose: true,
+            beforeShow: function(){
+                setTimeout(()=>{
+                    $("#ui-datepicker-div").css({
+                        position: "fixed",
+                        top: "50%",
+                        left: "50%",
+                        marginTop: "-50px",
+                        marginLeft: "-100px",
+                        zIndex: 2000,
+                    })
+                }, 10)
+            }
+        }    
 
-//   });
+        $('#bookingDatepicker').datepicker(options)
+
+        $( "#bookingDatepicker" ).datepicker( "option", "dateFormat", "D, d M y" );
+
+
+    
+
+    }
+}
